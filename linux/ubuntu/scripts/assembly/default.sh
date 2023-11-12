0300defbc95fcddc77f185d06fe55b1e0f7cea01
李盼庚 <lipg@outlook.com>
2023-11-12 21:09:53 +0800

    update actions

--- linux/ubuntu/scripts/assembly/default.sh
+++ linux/ubuntu/scripts/assembly/default.sh
@@ -4,11 +4,11 @@ base_dir="$(dirname "$(dirname "$0")")"
 # shellcheck source=./../helpers/function.sh
 source "$base_dir/helpers/function.sh"
 # shellcheck source=./../installers/base.sh
-source "$base_dir/base.sh"
+source "$base_dir/installers/base.sh"
 # shellcheck source=./../installers/node.sh
-source "$base_dir/node.sh"
+source "$base_dir/installers/node.sh"
 # shellcheck source=./../installers/docker.sh
-source "$base_dir/docker.sh"
+source "$base_dir/installers/docker.sh"
 
 # 清理临时文件
 clean_all
