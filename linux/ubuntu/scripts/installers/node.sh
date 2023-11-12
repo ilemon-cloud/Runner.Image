b1daa30c2777bf367a68453eb68bb0372c6a1ddc
李盼庚 <lipg@outlook.com>
2023-11-12 23:29:09 +0800

    update actions

--- linux/ubuntu/scripts/installers/node.sh
+++ linux/ubuntu/scripts/installers/node.sh
@@ -5,7 +5,7 @@ printf "\n\t🐋 Installing Node.JS=%s 🐋\t\n" "${node_version}"
 ver=$(curl https://nodejs.org/download/release/index.json | jq "[.[] | select(.version|test(\"^v${node_version}\"))][0].version" -r)
 node_path="$AGENT_TOOLSDIRECTORY/node/${ver:1}"
 mkdir -pv "${node_path}"
-wget "https://nodejs.org/download/release/latest-${ver}/node-$ver-linux-$(node_arch).tar.xz" -O "node-$ver-linux-$(node_arch).tar.xz"
+wget "https://nodejs.org/download/release/latest-${node_version}/node-$ver-linux-$(node_arch).tar.xz" -O "node-$ver-linux-$(node_arch).tar.xz"
 tar -Jxf "node-$ver-linux-$(node_arch).tar.xz" --strip-components=1 -C "$node_path"
 rm -rfv "node-$ver-linux-$(node_arch).tar.xz"
 sed "s|^PATH=|PATH=$node_path/bin:|mg" -i /etc/environment
